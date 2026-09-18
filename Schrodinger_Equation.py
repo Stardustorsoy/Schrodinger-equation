f5f7fe2136b9e3e776293a3a152a2995ef0125cc
Stardustorsoy <edwardhe78@gmail.com>
2026-09-17 23:24:59 -0400

    Added more comments, renamed some varaibles, and fixed minor calculation errors

--- Schrodinger_Equation.py
+++ Schrodinger_Equation.py
@@ -1,52 +1,71 @@
+#Import Numerical Python for high performance arrays, entire array calculations and for advance math
 import numpy as np
-import math
 
-ask_l = input("What sub-level do you want?")
 
-l = 0
-m = 0
+#Ask the user which sublevel they want
+ask_l = input("What sublevel do you want?")
 
+#If they entered the s sublevel, set the secondary quantum number (l) equal to 0
+#Automatically set the third quantum number (m) equal to 0 since there are no other orbitals that an electron could exist in the s sublevel
 if(ask_l == "s"):
     l = 0
     m = 0
 
+#If they entered the p sublevel, set the l equal to 1
 elif(ask_l == "p"):
     l = 1
+    #Created a boolean to track if the user entered a correct orbital
     done = False
     while done == False:
+
+        #Ask the user for the specific orbital they want
         m = int(input("Which orbital do want?"))
+
+        #Check if m is not valid for the p level (m is not equal to -1, 0 , or 1)
         if m != -1 and m != 0 and m != 1:
+            #Tell the user that their input was not valid and repeat the loop
             print("Not a valid orbital")
         else:
+            #Break the loop
             done = True
 
-voltage = np.zeros(2000)
-#Make an array for the radius. Starts from 0.01 and goes to 20 Bohr radius'. The value of the array are split into 1000 
+
+#Make an array to track the potential energy which is filled with 2000 zeros
+potential_energy = np.zeros(2000)
+
+#Make an array for the radius. Starts from 0.01 and goes to 20 Bohr radius'. The value of the array are split into 2000 pieces
 r = np.linspace(0.01,20,2000) 
 
-voltage = (-1.0/r) + (l*(l+1))/(2.0 * r ** 2)
+#Fill each value of the potential_energy array using this equation
+#The first bracket represents electrostatic attraction between the nucleus and electron
+#Typically the formula is PE = - kZe^2 / r, however since atomic number of hydrogen is 1 and atomic units are used, the numerator is just 1.0
+#The second bracket represents the centrifugal barrier
+#If the electron has a non 0 l-value, there exists true angular momentum from "rotating around the nucleus" which be accounted for
+#To find the rotational KE, combine the formulas L^2 = l(l+1) * h-bar , L^2 / 2I , and I = mr^2 to make l(l+1) / 2r^2 (m and h-bar are 1 since atomic units are used)
+potential_energy = (-1.0/r) + ((l*(l+1))/(2.0 * r ** 2))
 
-#Make the mass 1.0 electron mass
+#Set the mass equal to 1.0 electron mass
 mass_e = 1.0
-#Make h-bar 1.0 planck action
+
+#Set h-bar to 1.0 planck action
 h_bar = 1.0
+
 #Calculate the step size between two adjacent points
 h = r[1] - r[0]
+
 #Create an array to store the value of the wave function at each point
 psi = np.zeros(2000)
 
-energy = float(input("Enter an energy guess: "))
-#energy = -1/2n ** 2
+total_energy_guess = float(input("Enter an energy guess: "))
+#energy = -1/2 * n ** 2
 
 #Shooting Method
 psi[1] = 0.0001
 
 for i in range(1, 1999):
-    psi[i+1] = 2 * psi[i] - psi[i-1] - h ** 2 * 2 * (energy - voltage[i]) * psi[i]
-    
+    psi[i+1] = 2 * psi[i] - psi[i-1] - h ** 2 * 2 * (total_energy_guess - potential_energy[i]) * psi[i]
 
 print(psi[-1])
-
 
 
 grid_line = np.linspace(-10,10,100);
@@ -66,16 +85,19 @@ psi_1D = np.interp(distance, r, psi)
 
 #Spherical Harmonics
 if (l == 0):
-    sphe_Har = 1.0 / (4.0 * np.pi) ** 0.5
+    sphe_har = 1.0 / (4.0 * np.pi) ** 0.5
 elif (l == 1):
     if(m == -1):
-        sphe_Har = (3.0 / (8.0 * np.pi * np.sin(theta) * np.exp(-1j * phi))) ** 0.5
+        sphe_har = ((3.0 / (8.0 * np.pi)) ** 0.5) * np.sin(theta) * np.exp(-1j * phi)
     elif(m == 0):
-        sphe_Har = (3.0 / (4.0 * np.pi * np.cos(theta))) ** 0.5
+        sphe_har = ((3.0 / (4.0 * np.pi)) ** 0.5) * np.cos(theta)
     else:
-        sphe_Har = -(3.0 / (8.0 * np.pi * np.sin(theta) * np.exp(1j * phi))) ** 0.5
+        sphe_har = -((3.0 / (8.0 * np.pi))  ** 0.5) * np.sin(theta) * np.exp(1j * phi)
 
-psi_3D = psi_1D * sphe_Har
+#OR just use sphe_har = sph_harm(m, l, phi, theta)
+
+psi_3D = psi_1D * sphe_har
 
 probabilty_density = np.abs(psi_3D) ** 2.0
 
+print(psi_3D[-1])
